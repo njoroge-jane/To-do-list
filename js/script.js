b47eec93357640b075930a1e8892a8e3e28b1d2a
GraceMwende <grace.micheni@student.moringaschool.com>
2021-11-25 00:25:55 +0300

    able to delete task

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ function Add(task) {
 }
 // date function
 const day = new Date();
+
 $(document).ready(function () {
   // const refresh = $('.fa-refresh');
   const dateToday = $('#date');
@@ -14,14 +15,21 @@ $(document).ready(function () {
     event.preventDefault();
     var inputTask = $('#input').val();
 
+    // task object
     var newTask = new Add(inputTask);
-    if (!inputTask) {
+
+    if (inputTask == null || inputTask == "") {
       alert("Please fill a to do item");
       return false;
+
     } else {
       $(doList).append("<li><i class='fa fa-circle-thin complete'></i>" + newTask.addTask + "<i class='fa fa-trash-o delete position-absolute end-0 bg-danger text-white px-3 me-3 p-2'></i></li>");
       $('input[type="text"]').val(" ");
       return true;
     }
+  })
+  // delete a task
+  $(document).on("click", ".delete", function () {
+    $(this).remove();
   })
 })
