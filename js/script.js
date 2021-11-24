5303bc6464510efdd99ec2f99e4c31369207e914
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2021-11-24 18:38:24 +0300

    add hover effect on icons

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const refresh = document.querySelector('.fa-refresh');
+const dateToday = document.getElementById('date');
+const doList = document.getElementById('list');
+const input =document.getElementById('input').value
