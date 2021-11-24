342386c0c689b45b63927bb2bf0e6515f7a06a98
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2021-11-25 00:36:45 +0300

    make final changes to html

--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@ const refresh = document.querySelector('.fa-refresh');
 const dateToday = document.getElementById('date');
 const doList = document.getElementById('list');
 const input =document.getElementById('input').value
+const addButton = document.getElementsByTagName('button')
